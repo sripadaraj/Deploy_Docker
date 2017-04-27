a97c00fe011b99c6f4dc4695a133827452bec409
sripadaraj <sripadaraj183@gmail.com>
2017-04-27 08:11:20 +0530

    Comment lines are updated and need to be update

--- Volumes/filetest.java
+++ Volumes/filetest.java
@@ -1,17 +1,30 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
+//This program take a volume/directory as an input and prints the date and time it has been opened
+
+//PACKAGES:
+import java.util.*;
+import java.io.*;
+//Files input and output operations are enabled using IO package
 import java.nio.file.Paths;
-import java.io.IOException;
+//This package enables to make the root directory and to find path
+import java.sql.Timestamp;
+//It adds the ability to hold the SQL TIMESTAMP fractional seconds value,
+//by allowing the specification of fractional seconds to a precision of nanoseconds.
+import java.text.SimpleDateFormat;
+
 public class filetest
 {
  	
-	public static final String FILENAME = "/datadir/test.txt";
-
+	public static final String FILENAME = "/datadir/output.txt";
+       //store the location of the file directory 
 	public static void main(String a[])
 	{
+		File newTextFile = new File("/datadir/output.txt");
+		Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+                
+				
 		BufferedReader br = null;
 		FileReader fr = null;
-
+	//exection is done through the try and catch exceptions....
 		try {
 
 			fr = new FileReader(FILENAME);
@@ -46,8 +59,20 @@ while ((sCurrentLine = br.readLine()) != null) {
 
 		}
 
+try {
+            String str1 ="File was logged at time" ;
+	   String str2 = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+	
+            FileWriter fw = new FileWriter(newTextFile);
+            fw.write(str1);
+	    fw.write(str2);	
+            fw.close();
+
+        } catch (IOException iox) {
+            //do stuff with exception
+            iox.printStackTrace();
+        }
 	}
 
 }
-
 
